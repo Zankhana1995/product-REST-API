1df8ef5826184043f4ac5ff052cbdca46016ee16
Zankhana1995 <zankhanapatel1995.zp@gmail.com>
2020-01-16 23:12:14 -0500

    Spring Boot empty project added

---
+++ Spring-Boot-REST-API-Product/src/main/java/com/centric/api/product/SpringBootRestApiProductApplication.java
@@ -0,0 +1,13 @@
+package com.centric.api.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestApiProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestApiProductApplication.class, args);
+	}
+
+}
