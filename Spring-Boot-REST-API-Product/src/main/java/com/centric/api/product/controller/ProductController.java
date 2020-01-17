27938fad63bc9c125025d78289a6c7b55b9ea707
Zankhana1995 <zankhanapatel1995.zp@gmail.com>
2020-01-17 16:57:31 -0500

    Product controller created

---
+++ Spring-Boot-REST-API-Product/src/main/java/com/centric/api/product/controller/ProductController.java
@@ -0,0 +1,14 @@
+package com.centric.api.product.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+/**
+ * The Rest endpoint of the Product application
+ *
+ * @author Zankhana Patel
+ */
+@RestController
+@RequestMapping("/v1")
+public class ProductController {
+
+}
