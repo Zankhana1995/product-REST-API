178058d888ef22f8e4cd465a4d5dadcfee00bc96
Zankhana1995 <zankhanapatel1995.zp@gmail.com>
2020-01-18 00:33:56 -0500

    Hibernate annotation added for database connection

--- Spring-Boot-REST-API-Product/src/main/java/com/centric/api/product/model/Product.java
+++ Spring-Boot-REST-API-Product/src/main/java/com/centric/api/product/model/Product.java
@@ -4,18 +4,49 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
-import javax.persistence.Entity;
-import java.util.List;
+import org.hibernate.annotations.CreationTimestamp;
 
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+import java.util.List;
+/**
+ * The Model class of Product
+ *
+ * @author Zankhana Patel
+ */
 @Getter
 @Setter
 @NoArgsConstructor
 @ToString
-public class Product {
+@Entity
+@Table(name="products")
+public class Product implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", updatable = false)
+    private long id;
+
+    @Column(name = "name", length = 50, nullable = false)
     private String name;
+
+    @Column(name = "desc", length = 100)
     private String description;
+
+    @Column(name = "brand", length = 50)
     private String brand;
-    private List<String> tags;
+   // private List<String> tags;
+
+    @Column(name = "category", length = 50)
     private String category;
-    
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at", nullable = true)
+    private Date createdAt;
+
+
 }
