b2f6ac4e9251fffc55817f38ae7463a3e7bbb637
Zankhana1995 <zankhanapatel1995.zp@gmail.com>
2020-01-19 14:44:48 -0500

    Changes in ProductRepository class

--- Spring-Boot-REST-API-Product/src/main/java/com/centric/api/product/repository/ProductRepository.java
+++ Spring-Boot-REST-API-Product/src/main/java/com/centric/api/product/repository/ProductRepository.java
@@ -2,6 +2,7 @@ package com.centric.api.product.repository;
 
 import com.centric.api.product.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -10,5 +11,6 @@ import org.springframework.stereotype.Repository;
  * @author Zankhana Patel
  */
 @Repository
-public interface ProductRepository extends JpaRepository<Product,Long> {
+public interface ProductRepository extends JpaRepository<Product,Long>, JpaSpecificationExecutor<Product> {
+
 }
