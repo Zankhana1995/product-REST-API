35fa8289cbaffb31f5eb98b0fc2ea90bfe2e5a50
Zankhana1995 <zankhanapatel1995.zp@gmail.com>
2020-01-19 14:47:31 -0500

    rest endpoint changes in ProductController

--- Spring-Boot-REST-API-Product/src/main/java/com/centric/api/product/controller/ProductController.java
+++ Spring-Boot-REST-API-Product/src/main/java/com/centric/api/product/controller/ProductController.java
@@ -1,7 +1,14 @@
 package com.centric.api.product.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.centric.api.product.model.Product;
+import com.centric.api.product.repository.ProductRepository;
+import com.centric.api.product.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
 /**
  * The Rest endpoint of the Product application
  *
@@ -10,5 +17,44 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/v1")
 public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @Autowired
+    ProductService productService;
+
+
+    /**
+     * Get all product list.
+     *
+     * @return the list
+     */
+    @GetMapping("/products/search/findByCategory")
+    public List<Product> getAllProducts(
+
+            @RequestParam("category") String category,
+            @RequestParam(defaultValue = "0") Integer pageNo,
+            @RequestParam(defaultValue = "10") Integer pageSize,
+            @RequestParam(defaultValue = "createdAt") String sortBy
+
+    ) {
+
+        return productService.getAllProducts(category,pageNo,pageSize,sortBy);
+
+
+    }
+
+    /**
+     * Create Product.
+     *
+     * @param product the product
+     * @return the user
+     */
+    @PostMapping("/product")
+    public Product createProduct(@Valid @RequestBody Product product) {
+        return productRepository.save(product);
+    }
+
 
 }
