8faf577015adad962cc6fb10fdd2b6cd51cf4807
Zankhana1995 <zankhanapatel1995.zp@gmail.com>
2020-01-20 04:06:45 -0500

    Deleted sql file

--- Spring-Boot-REST-API-Product/src/test/java/com/centric/api/product/controller/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO PRODUCT (id, name, desc,brand,category) VALUES
-  ('b6afac37-cf9a-4fd4-8257-f096dbb5d34d', 'Red Shirt', 'Red hugo boss shirt','Hugo Boss','apparel'),
-  ('b6afac37-cf9a-4fd4-8257-f096dbb5d34e', 'blue Shirt', 'blue hugo boss shirt','Hugo Boss','apparel'),
-  ('b6afac37-cf9a-4fd4-8257-f096dbb5d34f', 'yellow Shirt', 'yellow hugo boss shirt','Hugo Boss','apparel');
